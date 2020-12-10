a8689e0c366b044133e2d114d3eb69c57ee4ce34
omjimishraa <70954767+omjimishraa@users.noreply.github.com>
2020-12-10 14:02:24 +0530

    Update sites.js

--- sites.js
+++ sites.js
@@ -5,3 +5,13 @@ $('.nav-tabs a').on('click', function(e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+$(document).bind("contextmenu", function(e) {
+    e.preventDefault();
+});
+
+$(document).keydown(function(e) {
+    if (e.which === 123) {
+        return false;
+    }
+});
